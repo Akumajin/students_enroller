3fdd69f156e576aacf86bb2bd4c1cb51788e68dd
Akumajin <azor_bet@yahoo.com>
2018-03-16 15:17:25 +0330

    Improved database creation process

--- src/classes/Database.php
+++ src/classes/Database.php
@@ -13,23 +13,33 @@ class Database
             $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
             $sql ="CREATE TABLE IF NOT EXISTS users (
                 id int(11) AUTO_INCREMENT PRIMARY KEY,
-                username varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
-                full_name varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-                password varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
-            );
-            CREATE TABLE IF NOT EXISTS modules (
+                username varchar(20) NOT NULL UNIQUE,
+                full_name varchar(20) NOT NULL,
+                password varchar(100) NOT NULL) ENGINE=InnoDB;
+            CREATE TABLE IF NOT EXISTS units (
                 id int(11) AUTO_INCREMENT PRIMARY KEY,
-                title varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-                description text COLLATE utf8mb4_unicode_ci,
-                credits int(11) NOT NULL);
+                title varchar(200) NOT NULL,
+                unit_code varchar(50) NOT NULL,
+                credits int(11) NOT NULL) ENGINE=InnoDB;
             CREATE TABLE IF NOT EXISTS enrollments(
                 id INT(11) AUTO_INCREMENT PRIMARY KEY,
                 user_id int(11) NOT NULL,
-                module_id int(11) NOT NULL,
+                unit_id int(11) NOT NULL,
                 create_date_time datetime NOT NULL,
-                UNIQUE KEY (module_id, user_id),
-                CONSTRAINT FK_User FOREIGN KEY (user_id) REFERENCES users(id),
-                CONSTRAINT FK_Module FOREIGN KEY (module_id) REFERENCES modules(id));";
+                UNIQUE KEY (unit_id, user_id),
+                FOREIGN KEY (user_id) REFERENCES users(id),
+                FOREIGN KEY (unit_id) REFERENCES units(id)) ENGINE=InnoDB;
+            INSERT INTO units (id, title, unit_code, credits) VALUES
+                (1, 'Academic Listening and Speaking', 'LANG10007', 10),
+                (2, 'Academic Reading and Writing', 'LANG10008', 10),
+                (3, 'Communicating Science', 'PHYS10001', 20),
+                (4, 'Contemporary European Cinema', 'MODL10009', 20),
+                (5, 'Historical Studies 1: Western Art Music (up to 1750)', 'MUSI10045', 20),
+                (6, 'Introduction to Cognitive Psychology', 'PSYC10006', 10),
+                (7, 'Living Religions', 'THRS10028', 20),
+                (8, 'Principles of Economics', 'EFIM10050', 20),
+                (9, 'The Archaeology of Myth: From the Trojan War to the end of Atlantis', 'CLAS12384', 20),
+                (10, 'Understanding Crime, Harm and Society', 'SPOL10020', 20);";
              $db->exec($sql);
         } catch(PDOException $e) {
             echo $e->getMessage();
